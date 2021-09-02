00b0f5e0e466f98d3d461f99082df1a23ea4ebfa
dingchun9527 <dingchun9527@163.com>
2021-09-03 00:05:52 +0800

    feat: and test assert & and gitingore

--- 1/plus_two_number_test.go
+++ 1/plus_two_number_test.go
@@ -1,10 +1,15 @@
 package plusTwoNumber
 
-import "testing"
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
 
 func TestTwoSum(t *testing.T) {
 	array := []int{2,7,11,15}
 	target := 9
 	index := TwoSum(array, target)
-	t.Log(index)
+	assert.Equal(t, 2, len(index))
+	assert.Equal(t, 0, index[0])
+	assert.Equal(t, 1, index[1])
 }
