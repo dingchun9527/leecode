70de65024bff46df12ccfd2c6943b58c3e23bf3b
dingchun <dingchun@wxblockchain.com>
2021-09-06 16:39:30 +0800

    feat: add test for subject 5

--- 5/longestPalindrome_test.go
+++ 5/longestPalindrome_test.go
@@ -1,1 +1,14 @@
 package longestPalindrome
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestLongestPalindrome(t *testing.T) {
+	longest := LongestPalindrome("abcdefg")
+	assert.Equal(t, "a", longest)
+	longest = LongestPalindrome("abcdefgfed")
+	assert.Equal(t, "defgfed", longest)
+
+}
