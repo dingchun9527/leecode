1a8aabdee114cc5c4b5b9ed01031310ed062f86e
dingchun9527 <dingchun9527@163.com>
2021-09-07 18:25:27 +0800

    fix: rewrite subject 15

--- 15/three_sum.go
+++ 15/three_sum.go
@@ -11,40 +11,42 @@ func ThreeSum(nums []int) [][]int {
 		return [][]int{}
 	}
 
-	// 先排序
+	// 排序
 	sort.Sort(sort.IntSlice(nums))
 
-	results := make([][]int, 0)
-	length, sum := len(nums), 0
+	results, sum, length := make([][]int, 0), 0, len(nums)
+
 	// 遍历数组
 	for i := 0; i < length; i++ {
-		// 当前元素已大于0, 和后面的元素相加不可能等于0
+		// 如果当前元素已经大于0, 不可能和更大的元素相加得到0
 		if nums[i] > 0 {
 			break
 		}
 
-		// 跳过重复
+		// 剔除重复的情况
 		if i > 0 && nums[i] == nums[i-1] {
 			continue
 		}
 
+		// 双指针获取后两个元素
 		left, right := i+1, length-1
 		for left < right {
 			sum = nums[i] + nums[left] + nums[right]
-			if sum == 0 { // sum正好等于0, 剔除重复, 寻找下一个
-				results = append(results, []int{nums[i], nums[left], nums[right]})
-				for left < right && nums[right] == nums[right-1] {
+			if sum == 0 { // sum刚好等于0, 需要往后移动第二个元素(变大), 往左移动第三个元素(变小)
+				results = append(results, []int{nums[i], nums[left], nums[right]}) // 加入结果集
+				if left < right && nums[right] == nums[right-1] {                  // 剔除重复元素
 					right--
 				}
 
 				for left < right && nums[left] == nums[left+1] {
 					left++
 				}
-				left++
+
+				left++  // 第二个元素右移
+				right-- // 第三个元素左移
+			} else if sum > 0 { // 第二个元素过小, 有移
 				right--
-			} else if sum > 0 { // sum大于0, 第三个元素需要减小
-				right--
-			} else { // sum小于0, 第二个元素需要加大
+			} else { // 第三个元素过大, 左移
 				left++
 			}
 		}
