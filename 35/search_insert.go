2280e14fd15c21ef7716c612ede260fa4a294f83
dingchun9527 <dingchun9527@163.com>
2021-09-26 17:09:55 +0800

    feat: submit subject 35

---
+++ 35/search_insert.go
@@ -0,0 +1,22 @@
+package searchInsert
+
+// 功能: 搜索插入位置
+// 要求: 时间复杂度为O(log n)
+
+
+func searchInsert(nums []int, target int) int {
+	length := len(nums)
+
+	l, r, mid, ans := 0, length - 1, 0, length
+
+	for l <= r {
+		mid = (l + r) / 2
+		if target <= nums[mid] {
+			ans = mid
+			r = mid - 1
+		} else {
+			l = mid + 1
+		}
+	}
+	return ans
+}
---
+++ 35/search_insert_test.go
@@ -0,0 +1,5 @@
+package searchInsert
+
+func searchInsert(nums []int, target int) int {
+	return 0
+}
